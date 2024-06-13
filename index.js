e6c014d9ce6478871cdcce452862cf2446b3ad5d
Aditto002 <adittokoiri002@gmail.com>
2024-06-13 20:44:54 +0600

    update

--- index.js
+++ index.js
@@ -32,3 +32,13 @@ app.listen(3000,() => {
 
 app.use("/api/user",userRoutes)
 app.use("/api/auth",authRouters)
+
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || " Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    });
+});
